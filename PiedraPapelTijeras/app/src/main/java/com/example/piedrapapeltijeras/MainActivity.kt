f05384496255931ed548f5a8a297d76732d990c8
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-10-27 14:32:41 +0200

    terminar

--- PiedraPapelTijeras/app/src/main/java/com/example/piedrapapeltijeras/MainActivity.kt
+++ PiedraPapelTijeras/app/src/main/java/com/example/piedrapapeltijeras/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.piedrapapeltijeras.ui.theme.PiedraPapelTijerasTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    tableroJuego()
                 }
             }
         }
@@ -34,16 +43,117 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Row {
-        Column {
-            Image(painter = painterResource(id = R.drawable.piedra), contentDescription = "Piedra")
+fun tableroJuego(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        Row(
+            modifier.weight(1f),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                modifier = modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.piedra),
+                    contentDescription = "Piedra",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+                Text(
+                    text = "Piedra",
+                    modifier = modifier.padding(5.dp)
+                )
+            }
+            Column(
+                modifier = modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.papel),
+                    contentDescription = "Papel",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+                Text(
+                    text = "Papel",
+                    modifier = modifier.padding(5.dp)
+                )
+            }
+            Column(
+                modifier = modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.tijeras),
+                    contentDescription = "Tijeras",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+                Text(
+                    text = "Tijeras",
+                    modifier = modifier.padding(5.dp)
+                )
+            }
         }
-        Column {
-            Image(painter = painterResource(id = R.drawable.papel), contentDescription = "Papel")
+
+        Row(
+            modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "HOLA")
         }
-        Column {
-            Image(painter = painterResource(id = R.drawable.tijera), contentDescription = "Piedra")
+
+        Row(
+            modifier.weight(1f),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.piedra),
+                    contentDescription = "Piedra",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+                Text(
+                    text = "Piedra",
+                    modifier = modifier.padding(5.dp)
+                )
+            }
+            Column(
+                modifier = modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.papel),
+                    contentDescription = "Papel",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+                Text(
+                    text = "Papel",
+                    modifier = modifier.padding(5.dp)
+                )
+            }
+            Column(
+                modifier = modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.tijeras),
+                    contentDescription = "Tijeras",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+                Text(
+                    text = "Tijeras",
+                    modifier = modifier.padding(5.dp)
+                )
+            }
         }
     }
 }
@@ -52,6 +162,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PiedraPapelTijerasTheme {
-        Greeting("Android")
+        tableroJuego()
     }
 }
