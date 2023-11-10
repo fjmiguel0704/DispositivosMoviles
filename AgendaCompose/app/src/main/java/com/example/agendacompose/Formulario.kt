5ecb87d172bc1a823ef8c9d5c40d437d7592f8fc
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-10 14:35:49 +0100

    terminar

---
+++ AgendaCompose/app/src/main/java/com/example/agendacompose/Formulario.kt
@@ -0,0 +1,4 @@
+package com.example.agendacompose
+
+class Formulario {
+}
--- AgendaCompose/app/src/main/java/com/example/agendacompose/MainActivity.kt
+++ AgendaCompose/app/src/main/java/com/example/agendacompose/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.agendacompose
 
 import android.os.Bundle
+import android.print.PrintAttributes.Margins
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,6 +61,7 @@ class MainActivity : ComponentActivity() {
                     Contacto("Pedro Gallardo", "665452423", "Hombre")
                 )
             )
+            botonFlotante()
         }
     }
     @Composable
@@ -56,30 +75,58 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun ContactoView (contacto: Contacto){
-        Card (Modifier.fillMaxWidth()){
-            Row {
-                Column {
-                    Image(
-                       painter = painterResource(id = R.drawable.iconperson),
-                        contentDescription = "Foto contacto",
-                        Modifier.height(100.dp)
-                    )
+        Card (modifier = Modifier.fillMaxWidth()
+            .background(Color.DarkGray)
+            .padding(10.dp)){
+            Column (Modifier.fillMaxSize()
+                    .background(Color.White)){
+                Row {
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.iconperson),
+                            contentDescription = "Foto contacto",
+                            Modifier.size(60.dp)
+                        )
+                    }
+
+                    Column {
+                        Text(
+                            text = contacto.nombre,
+                            fontSize = 16.sp,
+                            modifier = Modifier.padding(8.dp)
+                        )
+
+                        Text(
+                            text = contacto.tfno,
+                            fontSize = 16.sp,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
                 }
 
-                Column {
-                    Text(
-                        text = contacto.nombre,
-                        fontSize = 24.sp,
-                        modifier = Modifier.padding(8.dp)
-                    )
-
-                    Text(
-                        text = contacto.tfno,
-                        fontSize = 24.sp,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
             }
+        }
+    }
+
+    @Composable
+    fun botonFlotante (){
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.anadir),
+                contentDescription = "Add",
+                modifier = Modifier
+                    .size(60.dp)
+                    .align(Alignment.BottomEnd)
+                    .clip(CircleShape)
+                    .background(color = Color.LightGray)
+                    .clickable {
+
+                    }
+            )
         }
     }
 }
